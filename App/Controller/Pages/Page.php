31832fc3002c82610ea2981223315c00eb36e9c2
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2021-12-18 11:03:51 -0300

    Added the Page controller, responsible for being the main controller of the application

---
+++ App/Controller/Pages/Page.php
@@ -0,0 +1,22 @@
+<?php
+
+class Page {
+
+    private static function getHeader() {
+        return View::render('pages/header');
+    }
+
+
+    public static function getPage($title, $content) {
+        return View::render('pages/page', [
+            'title' => $title,
+            'header' => self::getHeader(),
+            'content' => $content,
+            'footer' => self::getFooter()
+        ]);
+    }
+
+    private static function getFooter() {
+        return View::render('pages/footer');
+    }
+}
