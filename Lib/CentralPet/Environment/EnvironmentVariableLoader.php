63c49276d63aa227bdec60ea5b66a7d24845344e
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2021-12-18 10:44:30 -0300

    Added class to load environment variables

---
+++ Lib/CentralPet/Environment/EnvironmentVariableLoader.php
@@ -0,0 +1,15 @@
+<?php
+namespace CentralPet\Environment;
+
+class EnvironmentVariableLoader {
+    public static function load($directory) {
+        if (!file_exists($directory.'/.env')) {
+            return false;
+        }
+
+        $variables = file($directory.'/.env');
+        foreach ($variables as $variable) {
+            putenv(trim($variable));
+        }
+    }
+}
