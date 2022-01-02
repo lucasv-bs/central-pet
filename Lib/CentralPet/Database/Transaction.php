6b78cf559485217fb1dfb60a3b9bf7dc7440bba6
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-01-02 00:14:02 -0300

    Created the Transaction class to manage application transactions on database

---
+++ Lib/CentralPet/Database/Transaction.php
@@ -0,0 +1,36 @@
+<?php
+namespace CentralPet\Database;
+
+final class Transaction {
+
+    private static $conn;
+
+    private function __construct() {}
+
+    public static function open() {
+        if(empty(self::$conn)) {
+            self::$conn = Connection::open();
+            self::$conn->beginTransaction();
+            return true;
+        }
+        return false;
+    }
+
+    public static function get() {
+        return self::$conn;
+    }
+
+    public static function close() {
+        if (self::$conn) {
+            self::$conn->commit();
+            self::$conn = null;
+        }
+    }
+
+    public static function rollback() {
+        if (self::$conn) {
+            self::$conn->rollback();
+            self::$conn = null;
+        }
+    }
+}
