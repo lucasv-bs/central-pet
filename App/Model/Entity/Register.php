fd3147a185e1e165ff8eb9f7a31b6bb606f000c7
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-02-01 12:52:57 -0300

    Added Register entity model class

---
+++ App/Model/Entity/Register.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Model\Entity;
+
+use App\Model\Entity\Owner;
+use App\Model\Entity\Pet;
+
+use CentralPet\Database\Connection;
+use CentralPet\Database\Repository;
+use CentralPet\Database\Transaction;
+
+class Register {
+    public static function setNewRegister($data) {
+        $pet = new Pet;
+        $owner = new Owner;
+
+        $pet->name = $data['pet_name'];
+        $pet->breed = $data['breed'];
+        $pet->birth_date = $data['pet_birth_date'];
+        $pet->last_vaccine_date = $data['last_vaccine_date'] ? $data['last_vaccine_date'] : '';
+        
+        $owner->name = $data['owner_name'];
+        $owner->birth_date = $data['owner_birth_date'];
+        $owner->document_number = $data['document_number'];
+        $owner->phone = $data['phone'];
+        $owner->email = $data['email'];
+        $owner->postal_code = $data['postal_code'];
+        $owner->address = $data['address'];
+        $owner->state = $data['state'];
+        $owner->city = $data['city'];
+
+        Transaction::open();
+        if ( !$pet->insert() || !$owner->insert() || !$owner->setPet($pet->id) ) {
+            Transaction::rollback();
+            return false;
+        }
+
+        Transaction::close();
+
+        return true;
+    }
+}
