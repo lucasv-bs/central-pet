3eba1c9e3bc4455d0bdf94b81f3c3cf104e12764
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-02-02 18:32:31 -0300

    Added setRegisterEdit method in Register entity model class

--- App/Model/Entity/Register.php
+++ App/Model/Entity/Register.php
@@ -38,4 +38,34 @@ class Register {
 
         return $pet->id;
     }
+
+
+    public static function setRegisterEdit($data, $pet_id) {        
+        $pet = Pet::getPetByValue($pet_id);        
+        $pet->name = $data['pet_name'];
+        $pet->breed = $data['breed'];
+        $pet->birth_date = $data['pet_birth_date'];
+        $pet->last_vaccine_date = $data['last_vaccine_date'] ? $data['last_vaccine_date'] : $pet->last_vaccine_date;
+        
+        $owner = $pet->getOwner();
+        $owner->name = $data['owner_name'];
+        $owner->birth_date = $data['owner_birth_date'];
+        $owner->document_number = $data['document_number'];
+        $owner->phone = $data['phone'];
+        $owner->email = $data['email'];
+        $owner->postal_code = $data['postal_code'];
+        $owner->address = $data['address'];
+        $owner->state = $data['state'];
+        $owner->city = $data['city'];
+
+        Transaction::open();
+        if ( !$pet->update() || !$owner->update() ) {
+            Transaction::rollback();
+            return false;
+        }
+
+        Transaction::close();
+        
+        return true;
+    }
 }
