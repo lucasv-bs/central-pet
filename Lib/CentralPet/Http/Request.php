ec75dc4d77e2f5ac8b09c35317cc9ba1a7cea57f
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-02-02 13:09:29 -0300

    Changed the way to set the URI and postVars. Added the router object and the getRouter method

--- Lib/CentralPet/Http/Request.php
+++ Lib/CentralPet/Http/Request.php
@@ -7,15 +7,41 @@ class Request {
     private $queryParams = [];
     private $postVars = [];
     private $headers = [];
+    private $router;
 
 
-    public function __construct() {
+    public function __construct($router) {
+        $this->router = $router;
         $this->queryParams = $_GET ?? [];
-        $this->postVars = $_POST ?? [];
         $this->header = getallheaders();
         $this->httpMethod = $_SERVER['REQUEST_METHOD'] ?? '';
-        $this->uri = $_SERVER['REQUEST_URI'] ?? '';
+        $this->setUri();
+        $this->setPostVars();
     }
+
+
+    private function setPostVars() {
+        if ($this->httpMethod == 'GET') return false;
+
+        $this->postVars = $_POST ?? [];
+
+        $inputRaw = file_get_contents('php://input');
+        $this->postVars = (strlen($inputRaw) && empty($_POST)) ? json_decode($inputRaw, true) : $this->postVars;
+    }
+
+
+    private function setUri() {
+        $this->uri = $_SERVER['REQUEST_URI'] ?? '';
+
+        $xUri = explode('?', $this->uri);
+        $this->uri = $xUri[0];
+    }
+
+
+    public function getRouter() {
+        return $this->router;
+    }
+
 
     public function getHttpMethod() {
         return $this->httpMethod;
