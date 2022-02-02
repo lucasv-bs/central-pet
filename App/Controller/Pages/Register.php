2e4d1ceb9b9e74ca28242834c6b4c8dce963f78c
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-02-02 12:38:55 -0300

    Changed the setNewRegister method to redirect to register edit page. Added the getRegisterEdit method

--- App/Controller/Pages/Register.php
+++ App/Controller/Pages/Register.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller\Pages;
 
+use App\Model\Entity\Owner;
+use App\Model\Entity\Pet;
 use App\Model\Entity\Register as EntityRegister;
 use App\View\View;
 
@@ -32,11 +34,40 @@ class Register Extends Page {
     public static function setNewRegister($request) {
         $data = $request->getPostVars();
 
-        if ( !EntityRegister::setNewRegister($data) ) {
+        $pet_id = EntityRegister::setNewRegister($data);
+        if ( !$pet_id ) {
             echo "Falha na inserção";
         }
-        else {
-            echo "Registro inserido com sucesso";
+        
+        $request->getRouter()->redirect("/register/{$pet_id}/edit?status=created");
+    }
+
+
+    public static function getRegisterEdit($request, $id) {
+        $pet = Pet::getPetByValue($id);
+        $owner = $pet->getOwner();
+
+        if (!$pet instanceof Pet || !$owner instanceof Owner) {
+            $request->getRouter()->redirect('/register');
         }
+
+        $content = View::render('pages/register', [
+            'title' => 'Central Pet',
+            'pet_name' => $pet->name,
+            'breed' => $pet->breed,
+            'pet_birth_date' => $pet->birth_date,
+            'last_vaccine_date' => $pet->last_vaccine_date,
+            'owner_name' => $owner->name,
+            'owner_birth_date' => $owner->birth_date,
+            'document_number' => $owner->document_number,
+            'phone' => $owner->phone,
+            'email' => $owner->email,
+            'postal_code' => $owner->postal_code,
+            'address' => $owner->address,
+            'state' => $owner->state,
+            'city' => $owner->city
+        ]);
+
+        return parent::getPage('Central Pet - Register', $content);
     }
 }
